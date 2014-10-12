ad1152d331f4c32e3f6ce009626b9d1c8887ced1
sshankar19 <sshankar19@gmail.com>
2014-10-11 20:59:28 -0400

    hello world state

---
+++ PebbleStadium/build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "PebbleStadium",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x6E, 0xF1, 0xD6, 0x5E, 0x00, 0xDC, 0x4A, 0x14, 0x9F, 0x68, 0x24, 0xAE, 0x9E, 0x98, 0xB5, 0xB9 },
+  .virtual_size = 0xb6b6
+};
